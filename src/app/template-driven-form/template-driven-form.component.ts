8dd4282f84a34c4ed4a3552ee7776f635245737b
Arul Kumaran25 <arulkumaran2519@gail.com>
2024-10-28 16:00:58 +0530

    Drop down functionalities added in the form

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template-driven-form',
   templateUrl: './template-driven-form.component.html',
   styleUrls: ['./template-driven-form.component.css']
 })
-export class TemplateDrivenFormComponent {
+export class TemplateDrivenFormComponent { 
+  countryList:Country[]=[
+    new Country('1','India'),
+    new Country('2','USA'),
+    new Country('3','England'),
+  ];
 
+  onSubmit(form:NgForm){
+    console.log(form);
+  
+  }
+
+}
+
+class Country{
+  id:string;
+  name:string;
+
+  constructor(id:string,name:string){
+    this.id=id;
+    this.name=name;
+
+  }
 }
